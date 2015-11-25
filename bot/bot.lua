e2b5dc79de3da67eefc7f16428e88f2adc1b000c
spark51 <dgalireza@gmail.com>
2015-11-25 05:38:28 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {spark051},
+    sudo_users = {spark051.75517656},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
